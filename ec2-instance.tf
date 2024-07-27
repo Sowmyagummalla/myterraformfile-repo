24c6594251d1b1c941615bae4925d97ea47ad88a
Sowmyagummalla <sowmyareddygummalla@gmail.com>
2024-07-27 14:09:11 +0530

    Add files via upload

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -1,8 +1,9 @@
 # Resource-8: Create EC2 Instance
 resource "aws_instance" "terraform-ec2-vm" {
-  ami = "ami-068e0f1a600cd311c" # Amazon Linux
+  ami = var.ec2_ami_id
   instance_type = "t2.micro"
   key_name = "clusterdev"
+  count = var.ec2_instance_count
   subnet_id = aws_subnet.vpc-new-public-subnet-1.id
   vpc_security_group_ids = [aws_security_group.new-vpc-sg.id]
   #user_data = file("apache-install.sh")
@@ -14,7 +15,7 @@ resource "aws_instance" "terraform-ec2-vm" {
     sudo systemctl start httpd
     echo "<h1>WELCOME TO AWS EC2 CREATION USING TERRAFORM </h1>" > /var/www/html/index.html
     EOF
-    tags = {
-      "Name" = "terraform-ec2-vm"
-    }  
+   tags = {
+    "Name" = "terraform-ec2-vm"
+   } 
 }
--- main.tf
+++ main.tf
@@ -8,5 +8,6 @@ terraform {
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = var.aws_region
+  profile = "default"
 }  
