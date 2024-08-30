5a96f63e6d6e0ffce9171dbdd9ed5416b7219546
Sowmyagummalla <sowmyareddygummalla@gmail.com>
2024-08-30 13:44:39 +0530

    Create var.tf

---
+++ Terraform-2/var.tf
@@ -0,0 +1,23 @@
+# Input Variables
+variable "aws_region" {
+  description = "Region in which AWS resources to be created"
+  type = string
+}
+
+variable "ec2_instance_type" {
+  description = "Type of AWS EC2 Instance"
+  type = string
+}
+
+variable "ec2_ami_id" {
+  description = "AMI ID"
+  type = string
+}
+
+variable "volume" {
+  description = "Root volume of the instance"
+  type = string 
+}
+
+
+Here It will ask you to provide the aws region and ec2 ami id and ec2 instance type and also volume
